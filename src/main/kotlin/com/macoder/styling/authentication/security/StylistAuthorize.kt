ef158984db2a50507fad29343d89d81f104fd4fd
isakchoe <isakchoe@naver.com>
2024-04-20 20:43:30 +0900

    feat - 스타일리스트 인가 어노테이션 구현

---
+++ src/main/kotlin/com/macoder/styling/authentication/security/StylistAuthorize.kt
@@ -0,0 +1,9 @@
+package com.macoder.styling.authentication.security
+
+import org.springframework.security.access.prepost.PreAuthorize
+
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@PreAuthorize("hasAuthority('STYLIST')")
+annotation class StylistAuthorize
